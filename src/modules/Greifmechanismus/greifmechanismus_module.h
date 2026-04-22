9b960353ead0bae7c86698862c5332b48cb147fb
Noé <twitch.g5n0e@gmail.com>
2026-04-22 08:00:00 +0200

    werte im headre standart uf 0 und 2.5 mueesen noch ermittelt werden

--- src/modules/Greifmechanismus/greifmechanismus_module.h
+++ src/modules/Greifmechanismus/greifmechanismus_module.h
@@ -11,36 +11,36 @@ constexpr float AUFNEHMEN_ABLEGEN_POS_BLAU_GRUEN_D = 0.344f;
 
 // Lenkung bei Aufnahme/Ablage am Haus fuer.
 constexpr float AUFNEHMEN_ABLEGEN_POS_tunnel_L = 0.25f;
-constexpr float AUFNEHMEN_ABLEGEN_POS_ROT_GELB_L = 0.15f;
-constexpr float AUFNEHMEN_ABLEGEN_POS_BLAU_GRUEN_L = 0.25f;
+constexpr float AUFNEHMEN_ABLEGEN_POS_ROT_GELB_L = 0.0f;
+constexpr float AUFNEHMEN_ABLEGEN_POS_BLAU_GRUEN_L = 0.0f;
 
 // Lagerplatz-Positionen am Drehkranz.
-constexpr float LAGER_POS_1_D = 0.10f;
-constexpr float LAGER_POS_2_D = 0.20f;
-constexpr float LAGER_POS_3_D = 0.30f;
-constexpr float LAGER_POS_4_D = 0.40f;
+constexpr float LAGER_POS_1_D = 0.0;
+constexpr float LAGER_POS_2_D = 0.0f;
+constexpr float LAGER_POS_3_D = 0.0f;
+constexpr float LAGER_POS_4_D = 0.0f;
 
 // Lenkung bei Lagerplaetzen.
-constexpr float LAGER_POS_1_L = 0.12f;
-constexpr float LAGER_POS_2_L = 0.18f;
-constexpr float LAGER_POS_3_L = 0.22f;
-constexpr float LAGER_POS_4_L = 0.28f;
+constexpr float LAGER_POS_1_L = 0.0f;
+constexpr float LAGER_POS_2_L = 0.0f;
+constexpr float LAGER_POS_3_L = 0.0f;
+constexpr float LAGER_POS_4_L = 0.0f;
 // false: einzelnes Paket; true: Paket wird zusaetzlich im Lager verwaltet.
 extern bool lager;
 
 // Seilhub fuer Haus und Lager.
 //rot_gelb
-constexpr float SEIL_ROTATIONEN_HAUS_ROT_GELB = 1.5f;
+constexpr float SEIL_ROTATIONEN_HAUS_ROT_GELB =2.5f;
 //blau_gruen
-constexpr float SEIL_ROTATIONEN_HAUS_BLAU_GRUEN = 1.8f;
+constexpr float SEIL_ROTATIONEN_HAUS_BLAU_GRUEN = 2.5f;
 //Lagerpos1
-constexpr float SEIL_ROTATIONEN_LAGER_POS_1 = 1.2f;
+constexpr float SEIL_ROTATIONEN_LAGER_POS_1 = 2.5f;
 //Lagerpos2
-constexpr float SEIL_ROTATIONEN_LAGER_POS_2 = 1.4f;
+constexpr float SEIL_ROTATIONEN_LAGER_POS_2 = 2.5f;
 //Lagerpos3
-constexpr float SEIL_ROTATIONEN_LAGER_POS_3 = 1.6f;
+constexpr float SEIL_ROTATIONEN_LAGER_POS_3 = 2.5f;
 //Lagerpos4
-constexpr float SEIL_ROTATIONEN_LAGER_POS_4 = 2.0f;
+constexpr float SEIL_ROTATIONEN_LAGER_POS_4 = 2.5f;
 
 }
 
